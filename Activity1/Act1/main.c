0f3ea3d099bc0df56e6a4271c8555dcd83a44bfc
fherbine <felix.herbinet@yahoo.com>
2018-06-04 23:53:49 +0200

    Adding activity1 project

---
+++ Activity1/Act1/main.c
@@ -0,0 +1,13 @@
+#include "stm32f10x.h"
+//#include "functions.h"
+
+int main(void){
+	int i;	
+//	static int observable;
+	
+	for(i=0;i<100000;i++){
+//		observable = function1(observable);
+	}
+//	function2();
+	return 0;
+}
