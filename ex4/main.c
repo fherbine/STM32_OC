afdbf03629ff5666c0f99d0a1de3e898d10dbef5
fherbine <felix.herbinet@yahoo.com>
2018-08-11 18:18:22 +0200

    Adding test on PIOs

--- ex4/main.c
+++ ex4/main.c
@@ -1,5 +1,8 @@
 #include <STM32f10x.h>
 
+#define TRUE 1
+#define FALSE 0
+
 typedef unsigned char		u8;
 typedef unsigned short	u16;
 typedef unsigned int		u32;
@@ -41,7 +44,36 @@ u32 porta_idr_status_new(void)
 	return (value);
 }
 
+
+void gpio_c3_config(void)
+{
+	GPIOC->CRL = GPIOC->CRL & ~(0xF << 4*3);
+	GPIOC->CRL = GPIOC->CRL | (0x3 << 4*3);
+}
+
+void open_close_gpio_c3(void)
+{
+	GPIOC->ODR = GPIOC->ODR ^ (1 << 3);
+}
+
+void basic_wait(void)
+{
+	u16 c = 0;
+	
+	while (c < 0xFF)
+		c++;
+}
+
 s32 main(void)
 {
+	RCC->APB2ENR = RCC->APB2ENR | RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPCEN;
+	
+	
+	gpio_c3_config();
+	open_close_gpio_c3();
+	
+	while (TRUE) {
+	open_close_gpio_c3();
+	}
 	return (0);
 }
