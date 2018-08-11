2a7fe236891ad00d265559a98e6f519adf1a82ff
fherbine <felix.herbinet@yahoo.com>
2018-08-11 17:07:17 +0200

    Adding a function to check GPIO port

--- ex4/main.c
+++ ex4/main.c
@@ -6,7 +6,16 @@ typedef signed char			s8;
 typedef signed short		s16;
 typedef signed int			s32;
 
-int main(void)
+
+u32	portb_idr_status(void)
+{
+	u32 value;
+	
+	value = *(u32 *)0x40010C08;
+	return (value);
+}	
+
+s32 main(void)
 {
 	return (0);
 }
