9cbefa6194ca21d04c6ef6652609c5cd475721c4
fherbine <felix.herbinet@yahoo.com>
2018-08-11 17:14:44 +0200

    Adding a function to check GPIO port B ODR status

--- ex4/main.c
+++ ex4/main.c
@@ -7,13 +7,26 @@ typedef signed short		s16;
 typedef signed int			s32;
 
 
+/*	Simple value checking on IDR port B	*/
+
 u32	portb_idr_status(void)
 {
 	u32 value;
 	
-	value = *(u32 *)0x40010C08;
+	value = *(u32 *)0x40010C08; // RM0008 - ref. man p.50 ==> address for Input Data Register for port B (offset 0x08)
 	return (value);
-}	
+}
+
+
+/*	Simple value checking on ODR port B	*/
+
+u32	portb_odr_status(void)
+{
+	u32 value;
+	
+	value = *(u32 *)0x40010C0C; // Same for Output Data register (ODR) for GPIO port B
+	return (value);
+}
 
 s32 main(void)
 {
