ce3a4e39d9d4449e336c2aa25ea83ef23cf69aa3
fherbine <felix.herbinet@yahoo.com>
2018-08-13 16:41:23 +0200

    Adding routine functions for tests IHM

--- ex5/main.c
+++ ex5/main.c
@@ -11,10 +11,25 @@ typedef signed char			s8;
 typedef signed short		s16;
 typedef signed int			s32;
 
-s32	main(void)
+void	config_gpio_c3(void)
+{
+	GPIOC->CRL &= ~(0xF << 12);
+	GPIOC->CRL |= (0x3 << 12);
+}
+
+void	config_usr_but(void)
+{
+	GPIOC->CRH &= ~(0xF << 20);
+	GPIOC->CRH |= (0x8 << 20);
+}
+
+s32		main(void)
 {
 	RCC->APB2ENR = RCC->APB2ENR | RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPCEN; // mandatory to use GPIOs
 	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN; // mandatory to use timer2
+	
+	config_gpio_c3();
+	config_usr_but();
 	
 	while (TRUE)
 	{
