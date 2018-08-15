c208c4c8c36af5f66c8cff7439a7902c532e957b
fherbine <felix.herbinet@yahoo.com>
2018-08-15 15:56:03 +0200

    Adding usefull files

---
+++ ex7/main.c
@@ -0,0 +1,7 @@
+#include "uart.h"
+
+s32 main(void)
+{
+	while (TRUE);
+	return(EXIT_SUCCESS);
+}
---
+++ ex7/uart.h
@@ -0,0 +1,20 @@
+#ifndef UART_H
+# define UART_H
+
+# include <STM32f10x.h>
+
+# define TRUE 1
+# define FALSE 0
+#	define EXIT_SUCCESS 0
+# define EXIT_FAILURE -1
+
+typedef unsigned char		u8;
+typedef unsigned short	u16;
+typedef unsigned int		u32;
+
+typedef signed char			s8;
+typedef signed short		s16;
+typedef signed int 			s32;
+
+
+#endif
